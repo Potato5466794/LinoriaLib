551b1fa904ee4ae6ac0b018d685d4bb98165d830
Nol1 <zhangyuguozhang4@gmail.com>
2026-03-20 13:07:35 +0800

    Update ThemeManager.lua

--- addons/ThemeManager.lua
+++ addons/ThemeManager.lua
@@ -213,7 +213,7 @@ local ThemeManager = {} do
 	end
 
 	function ThemeManager:LoadDefault()
-		local theme = 'Bbot'
+		local theme = 'BBot'
 		local content = isfile(self.Folder .. '/themes/default.txt') and readfile(self.Folder .. '/themes/default.txt')
 
 		local isDefault = true
